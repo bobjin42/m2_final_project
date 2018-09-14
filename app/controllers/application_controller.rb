7d0e88cb60da3d29b10db9656f337f8296e7a225
github username <github email address>
2018-09-13 21:07:38 -0400

    2107

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception, prepend: true
     include SessionsHelper
 
+    def tips
+
+    end
 
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,13 +5,15 @@ class StoresController < ApplicationController
     @stores = Store.all
   end
 
+  def tips
+
+  end
+
   def show
     @store = Store.find(params[:id])
     session[:store_id] = @store.id
   end
 
-  def tips
 
-  end
 
 end
